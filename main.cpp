d62ead06843204e5e8730bd221e63d2c5a82177e
Daniil <daniel217032001@gmail.com>
2020-10-08 00:27:21 +0300

    add example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <ctime>
 
+#include "libraries/merge.h"
 
 using std::cout;
 
 int main() {
+    srand(time(0));
 
+    unsigned int length = 10;
+    int* array = new int[length];
+    for (unsigned int i = 0; i < length; ++i) {
+        array[i] = rand() % 50;
+        cout << array[i] << " ";
+    }
+    cout << "\n";
+
+    merge_sort(array, length);
+
+    for (unsigned int i = 0; i < length; ++i) {
+        cout << array[i] << " ";
+    }
+
+    delete[] array;
     return 0;
 }
